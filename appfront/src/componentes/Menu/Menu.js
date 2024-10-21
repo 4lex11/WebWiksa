d293361ef123d9be1848cd63d53c972f2754392e
4lex11 <95388653+4lex11@users.noreply.github.com>
2024-10-21 16:20:20 -0500

    Update Menu.js

--- appfront/src/componentes/Menu/Menu.js
+++ appfront/src/componentes/Menu/Menu.js
@@ -12,8 +12,17 @@ export default function Menu() {
     <header className={classes.header}>
       <div className={classes.container}>
         <Link to="/" className={classes.logo}>
-          WIKSA 
+          WIKSA
         </Link>
+
+        <div className={classes.buscardor}>
+          <input type="text" placeholder="Buscar productos"/>
+          <button>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
+            </svg>
+            </button>
+        </div>
 
         <nav>
           <ul>
