11e56474891fb3b740a76202605f58e44a07adda
4lex11 <95388653+4lex11@users.noreply.github.com>
2024-10-20 23:08:21 -0500

    Create AppRoutes.js

---
+++ appfront/src/AppRoutes.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Routes,Route } from 'react-router-dom'
+import Inicio from './paginas/inicio/inicio';
+import ProductoPage from './paginas/Producto/Producto';
+import CategoriaPage from './paginas/Categoria/categoria';
+import VendedorPage from './paginas/Vendedor/Vendedor';
+
+export default function AppRoutes() {
+  return (
+  <Routes>
+    <Route path="/" element={ <Inicio/>} />
+    <Route path="/categoria/:name" element={CategoriaPage}/>
+    <Route path="/vendedor/:name" element={VendedorPage}/>
+    <Route path="/producto/:id" element={ProductoPage}/>
+  </Routes>
+  );
+}
