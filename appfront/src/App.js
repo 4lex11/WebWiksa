0ac16928f8d033b5d77f909e16b4f0848754f0cb
4lex11 <95388653+4lex11@users.noreply.github.com>
2024-10-20 23:08:19 -0500

    Create App.js

---
+++ appfront/src/App.js
@@ -0,0 +1,14 @@
+import AppRoutes from "./AppRoutes";
+import Menu from "./componentes/Menu/Menu";
+
+function App() {
+  return (
+    <>
+      <Menu/>
+      <AppRoutes/>
+    </>
+    
+  );
+}
+
+export default App;
