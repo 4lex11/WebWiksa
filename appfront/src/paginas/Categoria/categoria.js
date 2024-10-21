79a1783b1a42d6331588ef95c998eaa3448b2fff
4lex11 <95388653+4lex11@users.noreply.github.com>
2024-10-20 23:08:33 -0500

    Create categoria.js

---
+++ appfront/src/paginas/Categoria/categoria.js
@@ -0,0 +1,19 @@
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom';
+import { getAllBycategory } from '../../services/comidaservice';
+
+export default function CategoriaPage() {
+
+  const [categoria, setcategory] = useState({});
+  const {name} = useParams();
+
+  useEffect(()=>{
+    getAllBycategory(name).then(setcategory);
+  },[name]);
+
+  return (
+    <div>
+      PAGCATEGIR
+    </div>
+  )
+}
