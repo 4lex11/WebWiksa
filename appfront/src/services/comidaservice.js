8dec488bf1b8fd34a7789b966f7352149b8804a9
4lex11 <95388653+4lex11@users.noreply.github.com>
2024-10-21 11:44:50 -0500

    Update comidaservice.js

--- appfront/src/services/comidaservice.js
+++ appfront/src/services/comidaservice.js
@@ -1,18 +1,23 @@
 import { categorias, productos, vendedores } from '../data';
 
+// * LISTAR TODOS LOS PRODUCTOS
 export const Allproductos = async () => productos;
 
 export const searh = async searhTerm =>
     productos.filter();
 
+// * LISTAR TODAS LAS CATEGORIAS
 export const getAllcategorys = async () => categorias;
 
+// * LISTAR TODOS LOS VENDEDORES
 export const getAllvendedores = async () => vendedores;
 
+// * FILTRAR POR CATEGORIA
 export const getAllBycategory = async categoria =>{
     if(categoria ===  'All') return getAllvendedores();
     return vendedores.filter(item => item.category?.includes(categoria));
 }
 
+// * FILTRAR PRODUCTOS POR ID
 export const getById = async productoId => 
     productos.find(item => item.id === productoId);
