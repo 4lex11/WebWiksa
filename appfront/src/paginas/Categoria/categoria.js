8925f6d5b741895b6dd7defcb6f92b757776f5dc
4lex11 <95388653+4lex11@users.noreply.github.com>
2024-10-21 15:50:26 -0500

    Update categoria.js

--- appfront/src/paginas/Categoria/categoria.js
+++ appfront/src/paginas/Categoria/categoria.js
@@ -2,15 +2,27 @@ import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
 import { getAllBycategory } from '../../services/comidaservice';
 import classes from './categoria.module.css'
+import { vendedores } from '../../data';
 
-export default function CategoriaPage() {
+const initialState = { vendedores:[] };
 
-  const [categoria, setcategory] = useState({});
+const reducer = (state,action) =>{
+  switch(action.type){
+    case 'VENDEDORES_CARGADO':
+      return {...state, vendedores: action.payload};
+    default:
+      return state;
+  }
+}
+
+export default function CategoriaPage({vendedores}) {
+
+  /*const [categoria, setcategory] = useState({});
   const {name} = useParams();
 
   useEffect(()=>{
     getAllBycategory(name).then(setcategory);
-  },[name]);
+  },[name]);*/
 
   return (
     <>
